59b1ec437b8cf37a29666ff327948252fee04d7d
Jeiver YR <je.yande@udla.edu.co>
2024-10-29 22:04:16 -0500

    trabajo en clase

---
+++ tema15-entregable/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.css";
+
+//import NavBar from '../NavBar/NavBar';
+
+const Header = () => {
+  return (
+    <header>
+      <img id='image-movie' src="https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_1.5/c_scale,w_400/ncom/software/switch/70010000002732/desc/c855db8b24e1b8d6cabeee3ea2cb36422bc263d8b15223be9425c239450686d9" alt="" />
+      <div id='nombreid'>
+
+        <h1>Naruto</h1>
+        <p>- Información -</p>
+      </div>
+      {/* <NavBar /> */}
+    </header>
+  )
+}
+
+export default Header
